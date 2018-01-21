ceaa94d684311721dd60f7945847746b0f503eb8
Gilles Perreymond <gperreymond@gmail.com>
2018-01-21 16:59:30 +0100

    css homepage adjustments

--- client/views/Homepage/index.vue
+++ client/views/Homepage/index.vue
@@ -1,21 +1,29 @@
 <template>
-  <header>
-    <h1>Nova Platform</h1>
-    <h2>Homepage</h2>
-    <p>Overview of infrastructures.</p>
-    <h2>Servers: {{stats.servers.length}}</h2>
-    <ul>
-      <li v-for="item in stats.servers">{{item.uuid}}</li>
-    </ul>
-    <h2>Clients: {{stats.clients.length}}</h2>
-    <ul>
-      <li v-for="item in stats.clients">{{item.uuid}}</li>
-    </ul>
-    <h2>Services: {{stats.services.length}}</h2>
-    <ul>
-      <li v-for="item in stats.services">{{item.name}} ({{item.consumers}})</li>
-    </ul>
-  </header>
+  <div>
+    <header>
+      <h1>Nova Platform</h1>
+      <h3>Homepage</h3>
+      <p>Overview of infrastructures.</p>
+    </header>
+    <section>
+      <h2>Servers: {{stats.servers.length}}</h2>
+      <ul>
+        <li v-for="item in stats.servers">{{item.uuid}}</li>
+      </ul>
+    </section>
+    <section>
+      <h2>Clients: {{stats.clients.length}}</h2>
+      <ul>
+        <li v-for="item in stats.clients">{{item.uuid}}</li>
+      </ul>
+    </section>
+    <section>
+      <h2>Services: {{stats.services.length}}</h2>
+      <ul>
+        <li v-for="item in stats.services">{{item.name}} ({{item.consumers}})</li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script src="./script.js" />
